d74e1676107d2f8a9d029bb89beaac8296d515cf
MuliJob <munyoki912@gmail.com>
2025-03-04 12:41:17 +0300

    Added feed page

---
+++ mint/src/views/FeedView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+        <div class="main-center col-span-3 space-y-4">
+            <div class="bg-white border border-gray-200 rounded-lg">
+                <FeedForm 
+                    v-bind:user="null" 
+                    v-bind:posts="posts"
+                />
+            </div>
+
+            <div 
+                class="p-4 bg-white border border-gray-200 rounded-lg"
+                v-for="post in posts"
+                v-bind:key="post.id"
+            >
+                <FeedItem v-bind:post="post" v-on:deletePost="deletePost" />
+            </div>
+        </div>
+
+        <div class="main-right col-span-1 space-y-4">
+            <PeopleYouMayKnow />
+
+            <Trends />
+        </div>
+    </div>
+</template>
