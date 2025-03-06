94a47afb7bc0354cd61fc7155b69f1b427f930a4
MuliJob <munyoki912@gmail.com>
2025-03-06 15:22:20 +0300

    made it possible to send a message

--- backend/chat/api.py
+++ backend/chat/api.py
@@ -24,3 +24,42 @@ def conversation_detail(request, pk):
     serializer = ConversationDetailSerializer(conversation)
 
     return JsonResponse(serializer.data, safe=False)
+
+
+# @api_view(['GET'])
+# def conversation_get_or_create(request, user_pk):
+#     user = User.objects.get(pk=user_pk)
+
+#     conversations = Conversation.objects.filter(users__in=list([request.user])).filter(users__in=list([user]))
+
+#     if conversations.exists():
+#         conversation = conversations.first()
+#     else:
+#         conversation = Conversation.objects.create()
+#         conversation.users.add(user, request.user)
+#         conversation.save()
+
+#     serializer = ConversationDetailSerializer(conversation)
+    
+#     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['POST'])
+def conversation_send_message(request, pk):
+    """Sending a conversation message"""
+    conversation = Conversation.objects.filter(users__in=list([request.user])).get(pk=pk)
+
+    for user in conversation.users.all():
+        if user != request.user:
+            sent_to = user
+
+    conversation_message = ConversationMessage.objects.create(
+        conversation=conversation,
+        body=request.data.get('body'),
+        created_by=request.user,
+        sent_to=sent_to
+    )
+
+    serializer = ConversationMessageSerializer(conversation_message)
+
+    return JsonResponse(serializer.data, safe=False)
