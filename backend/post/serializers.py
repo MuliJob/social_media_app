fb261e0c12de4e6d12ef8fb060b64b60f4d33fa1
MuliJob <munyoki912@gmail.com>
2025-03-06 13:55:24 +0300

    created comments and post serializers

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from account.serializers import UserSerializer
-from .models import Post
+from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
     """Post serializer"""
@@ -15,3 +15,29 @@ class PostSerializer(serializers.ModelSerializer):
             'created_by',
             'created_at_formatted'
         )
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Comments model serializer"""
+    created_by = UserSerializer(read_only=True)
+
+    class Meta:
+        """Class meta"""
+        model = Comment
+        fields = ('id', 'body', 'created_by', 'created_at_formatted',)
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    """Post details serializer"""
+    created_by = UserSerializer(read_only=True)
+    comments = CommentSerializer(read_only=True, many=True)
+    # attachments = PostAttachmentSerializer(read_only=True, many=True)
+
+    class Meta:
+        """Class meta"""
+        model = Post
+        fields = ('id', 'body',
+                  'likes_count', 'comments_count',
+                  'created_by', 'created_at_formatted',
+                  'comments', 
+                #   'attachments',
+                  )
