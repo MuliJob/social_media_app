1e57c29105485565d9c0141f72a2e43507c3d181
MuliJob <munyoki912@gmail.com>
2025-03-07 13:35:27 +0300

    Getting suggested friends

---
+++ backend/scripts/generate_friend_suggestions.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import django
+import os
+import sys
+
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
+
+from account.models import User
+
+
+users = User.objects.all()
+
+for user in users:
+    # Clear the suggestion list
+    user.people_you_may_know.clear()
+
+    print('Find friends for:', user)
+
+    for friend in user.friends.all():
+        print('Is friend with:', friend)
+
+        for friendsfriend in friend.friends.all():
+            if friendsfriend not in user.friends.all() and friendsfriend != user:
+                user.people_you_may_know.add(friendsfriend)
+
+    print()
