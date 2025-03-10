977a40887d291320b51445d72039ff8896749055
MuliJob <munyoki912@gmail.com>
2025-03-10 19:29:52 +0300

    Removing lint errors

--- mint/src/components/CommentItem.vue
+++ mint/src/components/CommentItem.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="mb-6 flex items-center justify-between">
+    <div class="comment-container">
+      <div class="mb-6 flex items-center justify-between">
         <div class="flex items-center space-x-6">
-            <img :src="comment.created_by.get_avatar" class="w-[40px] rounded-full">
-            
-            <p>
-                <strong>
-                    <RouterLink :to="{name: 'profile', params:{'id': comment.created_by.id}}">{{ comment.created_by.name }}</RouterLink>
-                </strong>
-            </p>
+          <img :src="comment.created_by.get_avatar" class="w-[40px] rounded-full">
+          
+          <p>
+            <strong>
+              <RouterLink :to="{name: 'profile', params:{'id': comment.created_by.id}}">{{ comment.created_by.name }}</RouterLink>
+            </strong>
+          </p>
         </div>
-
+  
         <p class="text-gray-600">{{ comment.created_at_formatted }} ago</p>
+      </div>
+  
+      <p>{{ comment.body }}</p> 
     </div>
-
-    <p>{{ comment.body }}</p> 
-</template>
+  </template>
 
 <script>
 import { RouterLink } from 'vue-router'
